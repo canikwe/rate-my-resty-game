4485a6c4da9a6c203c96753721542cb98940f9c1
canikwe <c.anikwe@gmail.com>
2019-09-11 16:07:49 -0400

    set up redux store and connect initial components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from 'react'
 import Menu from './containers/Menu'
 import NewDishForm from './components/NewDishForm'
-import './App.css';
+import './App.css'
+
+//Redux-y things!
+import store from './redux/store/index'
+import { addMenu } from './redux/actions/index'
+
+window.store = store
+window.addMenu = addMenu
+
 
 const App = () => {
   return (
